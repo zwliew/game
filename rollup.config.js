7cc222a86b8681af85e1569ef7317221c11ded5b
Zhao Wei Liew <zhaoweiliew@gmail.com>
2020-05-30 10:24:10 +0800

    Regenerate templates

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,9 @@
 import merge from "deepmerge";
+// use createSpaConfig for bundling a Single Page App
 import { createSpaConfig } from "@open-wc/building-rollup";
+
+// use createBasicConfig to do regular JS to JS bundling
+// import { createBasicConfig } from '@open-wc/building-rollup';
 
 const baseConfig = createSpaConfig({
   // use the outputdir option to modify where files are output
@@ -7,7 +11,7 @@ const baseConfig = createSpaConfig({
 
   // if you need to support older browsers, such as IE11, set the legacyBuild
   // option to generate an additional build just for this browser
-  legacyBuild: true,
+  // legacyBuild: true,
 
   // development mode creates a non-minified build for debugging or development
   developmentMode: process.env.ROLLUP_WATCH === "true",
@@ -17,5 +21,11 @@ const baseConfig = createSpaConfig({
 });
 
 export default merge(baseConfig, {
+  // if you use createSpaConfig, you can use your index.html as entrypoint,
+  // any <script type="module"> inside will be bundled by rollup
   input: "./index.html",
+
+  // alternatively, you can use your JS as entrypoint for rollup and
+  // optionally set a HTML template manually
+  // input: './app.js',
 });
